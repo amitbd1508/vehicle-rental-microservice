45c0165d93ca27db515e8f8a8ef8796f04f940f0
Amit <amit.ghosh@cefalo.com>
2022-09-24 17:21:17 -0500

    rename package name

---
+++ account-service/src/main/java/com/carreservation/accountservice/repo/PaymentMethodRepo.java
@@ -0,0 +1,10 @@
+package com.carreservation.accountservice.repo;
+
+import com.carreservation.accountservice.entity.PaymentMethod;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PaymentMethodRepo extends JpaRepository<PaymentMethod,Long> {
+    Optional<PaymentMethod> getPaymentMethodByName(String name);
+}
