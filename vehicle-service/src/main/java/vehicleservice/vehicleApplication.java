60aa171031cb2c7d99ddbed0e5b9407da811e531
Silvana Ishak <silvana.ishak@miu.edu>
2022-09-24 21:37:19 -0500

    vehicle-service

---
+++ vehicle-service/src/main/java/vehicleservice/vehicleApplication.java
@@ -0,0 +1,11 @@
+package vehicleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class vehicleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(vehicleApplication.class, args);
+    }
+}
