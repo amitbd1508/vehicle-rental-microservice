ccbfd56b76cfd70d4d9bf0a79fa08c6c2fbf5b67
Silvana Ishak <silvana.ishak@miu.edu>
2022-09-26 17:29:27 -0500

    test

--- catalog-service/src/test/java/com/carreservation/catalogservice/ServiceTest.java
+++ catalog-service/src/test/java/com/carreservation/catalogservice/ServiceTest.java
@@ -1,9 +1,20 @@
 package com.carreservation.catalogservice;
 
+import com.carreservation.catalogservice.service.VehicleService;
+import com.carreservation.catalogservice.service.impl.VehicleServiceImp;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
+@RunnerWith(SpringRunner.class)
 public class ServiceTest {
 
+    @TestConfiguration
+    static class ServiceTestContextConfiguration {
+        @Bean
+        public VehicleService vehicleService() {
+            return new VehicleServiceImp();
+        }
+    }
 
 }
