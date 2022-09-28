ca35d19066777fb55bb3da5d744098b7980b1161
Amit <amit.ghosh@cefalo.com>
2022-09-28 05:00:33 -0500

    add deployemnt

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,16 @@ gcloud config set project possible-tape-183617
 mvn clean package  -DskipTests -X
 skaffold build
 skaffold run
+
+
+export GITHUB_REPO=amitbd1508/vehicle-rental-microservice
+export PROJECT_ID=possible-tape-183617
+export SERVICE_ACCOUNT=github-actions-vehicle-rental
+export WORKLOAD_IDENTITY_POOL=gh-pool
+export WORKLOAD_IDENTITY_PROVIDER=gh-provider
+
+echo $WORKLOAD_IDENTITY_PROVIDER_LOCATION
+projects/222904314823/locations/global/workloadIdentityPools/gh-pool/providers/gh-provider
+echo $SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com
+github-actions-vehicle-rental@possible-tape-183617.iam.gserviceaccount.com
+https://blog.leandrotoledo.org/deploying-google-cloud-functions-using-github-actions-and-workload-identity-authentication/
