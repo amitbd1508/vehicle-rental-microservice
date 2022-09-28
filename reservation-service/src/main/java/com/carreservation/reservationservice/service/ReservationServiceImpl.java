b69aa22dbad451e15e5499dbd0bf2631c00020c7
Abdul Kadir <akadir@wcgclinical.com>
2022-09-28 18:47:58 -0500

    payment refactor

--- reservation-service/src/main/java/com/carreservation/reservationservice/service/ReservationServiceImpl.java
+++ reservation-service/src/main/java/com/carreservation/reservationservice/service/ReservationServiceImpl.java
@@ -7,6 +7,7 @@ import com.carreservation.reservationservice.kafkamodels.CatalogMessageDTO;
 import com.carreservation.reservationservice.kafkamodels.PaymentRequestDTO;
 import com.carreservation.reservationservice.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -32,37 +33,23 @@ public class ReservationServiceImpl implements ReservationService{
     @Autowired
     private KafkaTemplate<String, CatalogMessageDTO> kafkaCatalogTemplate;
 
+    @Value("${catalog.Url}")
+    private String catalogUrl;
+
     @Override
     public Reservation addReservation(ReservationRequest reservationRequest, String vehicleId) {
 //        Account account = restTemplate.getForObject("http://localhost:8080/api/v1/users/" + reservationRequest.getAccount().getId(), Account.class);
-        Vehicle vehicle = new Vehicle(); //restTemplate.getForObject("http://localhost:9001/vehicles/getvehicle/" + vehicleId, Vehicle.class);
+        Vehicle vehicle = restTemplate.getForObject(catalogUrl+"/" + vehicleId , Vehicle.class);
 
-        vehicle.setVehicleStatus(VehicleStatus.AVAILABLE);
-        vehicle.setId("string-id");
-        vehicle.setBrand("Ford");
-        vehicle.setVehicleType(VehicleType.SEDAN);
-        vehicle.setColor("Silver");
-        vehicle.setPrice(62.55);
 
         if (vehicle.getVehicleStatus() == VehicleStatus.AVAILABLE) {
             Reservation reservation = new Reservation();
             reservation.setDuration(reservationRequest.getDuration());
             reservation.setAccount(reservationRequest.getAccount());
             reservation.setPaymentType(reservationRequest.getPaymentType());
-//            vehicle.setId(null);
             reservation.setVehicle(vehicle);
             reservation.setReservationStatus(ReservationStatus.PENDING);
-            reservation.getVehicle().setVehicleStatus(VehicleStatus.RESERVED);
             reservation.getVehicle().setId(vehicleId);
-            //restTemplate.put("http://localhost:9001/vehicles/update-status/" + vehicleId, VehicleStatus.RESERVED, VehicleStatus.class);
-            PaymentRequestDTO dto= new PaymentRequestDTO();
-            dto.setUserId(reservation.getAccount().getId());
-            dto.setAmount(reservation.getVehicle().getPrice());
-            dto.setPaymentType(reservation.getPaymentType().toString());
-            dto.setQueueId(reservation.getId());
-            dto.setEmail(reservationRequest.getAccount().getEmail());
-
-            kafkaTemplate.send(KafkaConfig.TOPIC_NAME_RESERVATION_CREATED, dto);
 
             return reservationRepository.save(reservation);
         } else
@@ -93,38 +80,32 @@ public class ReservationServiceImpl implements ReservationService{
         return reservationRepository.findById(reservationId).get();
     }
 
-//    @Override
-//    public Reservation updateStatus(Long reservationId, VehicleStatus status) {
-//        Reservation reservation= reservationRepository.findById(reservationId).get();
-//        reservation.setReservationStatus(ReservationStatus.RESERVED);
-//        return reservation;
-//    }
 
     @Override
     public String pay(PaymentRequest paymentRequest) {
-        Optional<Reservation> optionalReservation = reservationRepository.findById(paymentRequest.getReservationId());
-        if (!optionalReservation.isPresent()) {
+        Reservation reservation = reservationRepository.findById(paymentRequest.getReservationId()).orElse(null);
+        if (reservation== null||reservation.getReservationStatus().equals(ReservationStatus.CANCEL) || reservation.getReservationStatus().equals(ReservationStatus.RESERVED)) {
             System.out.println("No Reservation Found by id: " + paymentRequest.getReservationId());
-            return "Reservation is Not Successful";
+            return "Not a valid Reservation";
         }
 
-        Double price = optionalReservation.get().getVehicle().getPrice();
+        var vehical = restTemplate.getForObject(catalogUrl+"/" + reservation.getVehicle().getId(), Vehicle.class);
 
-        LocalDate startDate = optionalReservation.get().getDuration().getRentalDate();
-        LocalDate endDate = optionalReservation.get().getDuration().getReturnDate();
-        Double days = (double) ChronoUnit.DAYS.between(startDate, endDate);
-        Double totalPrice = price * days;
-        paymentRequest.setTotalPrice(totalPrice);
-        paymentRequest.setPaymentStatus(PaymentStatus.PENDING);
-
-        PaymentRequest paymentRequest1 = restTemplate.postForObject("http://localhost:9041/payments", paymentRequest, PaymentRequest.class);
-
-        if(paymentRequest1.getPaymentStatus() == PaymentStatus.PAID){
-            optionalReservation.get().setReservationStatus(ReservationStatus.RESERVED);
-            return "Payment processed successfully, reservation confirmed!";
-        } else {
-            return "Payment request failed, please try again!";
+        if(vehical.getVehicleStatus().equals(VehicleStatus.RESERVED)){
+            return "This Vehicle is not available";
         }
+
+        PaymentRequestDTO dto= new PaymentRequestDTO();
+        dto.setUserId(reservation.getAccount().getId());
+        dto.setAmount(reservation.getVehicle().getPrice());
+        dto.setPaymentType(reservation.getPaymentType().toString());
+        dto.setQueueId(reservation.getId());
+        dto.setEmail(reservation.getAccount().getEmail());
+
+        kafkaTemplate.send(KafkaConfig.TOPIC_NAME_RESERVATION_CREATED, dto);
+
+        return "Processing...";
+
     }
 
     public void UpdateReservationStatus(UpdateReservationMessage updateReservationMessage){
@@ -137,6 +118,7 @@ public class ReservationServiceImpl implements ReservationService{
                 CatalogMessageDTO catalogMessageDTO = new CatalogMessageDTO();
                 catalogMessageDTO.setVehicleId(reservations.getVehicle().getId());
                 catalogMessageDTO.setReservationStatus(ReservationStatus.RESERVED.toString());
+
                 kafkaCatalogTemplate.send(KafkaConfig.TOPIC_NAME_RESERVATION_CHANGED, catalogMessageDTO);
             }
         }else {
@@ -146,7 +128,7 @@ public class ReservationServiceImpl implements ReservationService{
 
                 CatalogMessageDTO catalogMessageDTO = new CatalogMessageDTO();
                 catalogMessageDTO.setVehicleId(reservations.getVehicle().getId());
-                catalogMessageDTO.setReservationStatus(ReservationStatus.CANCEL.toString());
+                catalogMessageDTO.setReservationStatus(VehicleStatus.AVAILABLE.toString());
                 kafkaCatalogTemplate.send(KafkaConfig.TOPIC_NAME_RESERVATION_CHANGED, catalogMessageDTO);
             }
         }
