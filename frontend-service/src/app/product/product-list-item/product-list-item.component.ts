a6afd41236b55891375c483083c1db7875d04ca4
Amit <amit.ghosh@cefalo.com>
2022-09-28 21:21:46 -0500

    open reservation for all

--- frontend-service/src/app/product/product-list-item/product-list-item.component.ts
+++ frontend-service/src/app/product/product-list-item/product-list-item.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../../shared/models/product';
+import {AccountService} from "../../account/account.service";
+import {ToastComponent} from "../../shared/components/toast/toast.component";
+import {ProductService} from "../product.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-product-list-item',
@@ -10,7 +14,41 @@ export class ProductListItemComponent implements OnInit {
   @Input() product: Product;
   quantity = 0;
 
-  constructor() {}
+  constructor(private accountService: AccountService,
+              private router: Router,
+              private service: ProductService, public toast: ToastComponent) {}
 
   ngOnInit(): void {}
+
+  reserve(product: Product) {
+    const token = localStorage.getItem('token');
+    if(!token) {
+      this.accountService.logout();
+      this.toast.setMessage(`User is logged out by system`, 'danger');
+    }
+    const user = localStorage.getItem('user');
+    if(!user){
+      this.toast.setMessage(`User is logged out by system!`, 'danger');
+    }
+    const parseedUser = JSON.parse(user);
+    const body = {
+      account: {
+        email: parseedUser.email,
+        id: parseedUser.id
+      },
+      "duration": {
+        "rentalDate": "10-10-2022",
+        "returnDate": "11-11-2022"
+      },
+      "paymentType": "BANK"
+    }
+
+    this.service.reserveProduct(token, body, product.id).subscribe(data => {
+      console.log("data ==", data);
+      this.toast.setMessage(`Car is reserved for 24 hours please make the payment!`, 'success');
+
+    });
+
+    // this.router.navigate(['/payment']);
+  }
 }
