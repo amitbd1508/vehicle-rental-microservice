7850a71fc3c0719d96ddb6ccd375b7309856813a
Amit <amit.ghosh@cefalo.com>
2022-09-28 19:38:58 -0500

    open reservation for all

--- reservation-service/src/main/java/com/carreservation/reservationservice/security/SecurityConfiguration.java
+++ reservation-service/src/main/java/com/carreservation/reservationservice/security/SecurityConfiguration.java
@@ -35,7 +35,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     http
       .csrf().disable()
       .authorizeRequests()
-      .antMatchers( HttpMethod.GET, "/").permitAll()
+      .antMatchers(  "/**").permitAll()
       .antMatchers("/v2/api-docs/**", "/swagger-ui/**", "/swagger-resources/**").permitAll()
       .anyRequest()
       .authenticated()
